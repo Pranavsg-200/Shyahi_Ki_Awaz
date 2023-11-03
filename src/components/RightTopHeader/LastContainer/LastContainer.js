b9c1dd0f16ceb512eff8ffc663f7e1e43aec8d49
Poonam229 <poonam.chauhan229@gmail.com>
2023-11-03 23:12:28 +0530

    rightcontainer completed

--- src/components/RightTopHeader/LastContainer/LastContainer.js
+++ src/components/RightTopHeader/LastContainer/LastContainer.js
@@ -20,63 +20,57 @@ function LastContainer() {
     }
   ]
   return (
-    <div>
-      <div>
-        <Card style={{ padding: "1",margin:"20px 10px 0px 10px",border:"none",backgroundColor:"#fff5f6" }}>
-          <Card.Body>
-            <Card.Title>SKA </Card.Title>
-            <Card.Text>
-              <b>Read in-depth stories by experts</b>
-            </Card.Text>
-            <ul>
-              <li>Will 16th finance panel widen the north-south divide?</li>
-              <li>Why Rahul's caste stand goes against Rajiv, Indira's</li>
-              <li>Does Centre take more but give less to Tamil Nadu?</li>
-              <li>Has bigotry blocked view of shared humanity in West Asia?</li>
-              <li>Will the Scindias survive or sink in Rajasthan and MP?</li>
-            </ul>
-          </Card.Body>
-        </Card>
-      </div>
+    <>
+     
+    <Card style={{ padding: "1",margin:"20px 0px 0px 10px",border:"none",backgroundColor:"#fff5f6" }}>
+      <Card.Body>
+        <Card.Title>SKA </Card.Title>
+        <Card.Text>
+          <b>Read in-depth stories by experts</b>
+        </Card.Text>
+        <ul>
+          <li>Will 16th finance panel widen the north-south divide?</li>
+          <li>Why Rahul's caste stand goes against Rajiv, Indira's</li>
+          <li>Does Centre take more but give less to Tamil Nadu?</li>
+          <li>Has bigotry blocked view of shared humanity in West Asia?</li>
+          <li>Will the Scindias survive or sink in Rajasthan and MP?</li>
+        </ul>
+      </Card.Body>
+    </Card>
 
-      <div>
-      <ListGroup style={{ padding: "1", margin: "20px 10px 0px 10px" }}>
-        {
-          listItem.map((element)=>(
-            <ListGroup.Item style={{border:"none",borderBottom:"1px dotted black"}}><li type="circle">{element.liItem.substring(0,43)+"....."}</li></ListGroup.Item>
-          ))
-        }
-      </ListGroup>
-      </div>
+    <ListGroup style={{ padding: "1", margin: "20px 10px 0px 10px" }}>
+      {
+        listItem.map((element)=>(
+          <ListGroup.Item style={{border:"none",borderBottom:"1px dotted black"}}><li type="circle">{element.liItem.substring(0,43)+"....."}</li></ListGroup.Item>
+        ))
+      }
+    </ListGroup>
+   
+    <Card style={{ padding: "1",margin:"20px 10px 0px 10px",border:"none",backgroundColor:"#fff5f6" }}>
+      <Card.Body>
+        
+        <Card.Text>
+          <b>Top 10 Stories</b>
+        </Card.Text>
+        <ul>
+          <li>Will 16th finance panel widen the north-south divide?</li>
+          <li>Why Rahul's caste stand goes against Rajiv, Indira's</li>
+          <li>Does Centre take more but give less to Tamil Nadu?</li>
+          <li>Has bigotry blocked view of shared humanity in West Asia?</li>
+          <li>Will the Scindias survive or sink in Rajasthan and MP?</li>
+        </ul>
+      </Card.Body>
+    </Card>
 
-      <div>
-        <Card style={{ padding: "1",margin:"20px 10px 0px 10px",border:"none",backgroundColor:"#fff5f6" }}>
-          <Card.Body>
-            
-            <Card.Text>
-              <b>Top 10 Stories</b>
-            </Card.Text>
-            <ul>
-              <li>Will 16th finance panel widen the north-south divide?</li>
-              <li>Why Rahul's caste stand goes against Rajiv, Indira's</li>
-              <li>Does Centre take more but give less to Tamil Nadu?</li>
-              <li>Has bigotry blocked view of shared humanity in West Asia?</li>
-              <li>Will the Scindias survive or sink in Rajasthan and MP?</li>
-            </ul>
-          </Card.Body>
-        </Card>
-      </div> 
-
-      <div>
-      <ListGroup style={{ padding: "1", margin: "20px 10px 0px 10px" }}>
-        {
-          listItem.map((element)=>(
-            <ListGroup.Item style={{border:"none",borderBottom:"1px dotted black"}}><li type="circle">{element.liItem.substring(0,43)+"....."}</li></ListGroup.Item>
-          ))
-        }
-      </ListGroup>
-      </div> 
-    </div>
+    <ListGroup style={{ padding: "1", margin: "20px 10px 0px 10px" }}>
+      {
+        listItem.map((element)=>(
+          <ListGroup.Item style={{border:"none",borderBottom:"1px dotted black"}}><li type="circle">{element.liItem.substring(0,43)+"....."}</li></ListGroup.Item>
+        ))
+      }
+    </ListGroup>
+     
+    </>
   );
 }
 
