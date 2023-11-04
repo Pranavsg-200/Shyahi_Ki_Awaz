c3d48596ab3dfa026e48b29e7cfaa2d5cf12040f
Pranavsg-200 <pranavsg2002@gmail.com>
2023-11-04 13:39:09 +0530

    Left Part Done

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,38 +1,38 @@
-import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
-import RightTopHeaderConatiner from "./RightTopHeader/RightTopHeaderConatiner";
+import { Container, Row, Col, Card, Carousel } from "react-bootstrap";
+import React, { useState } from "react";
 import Sample_Report from "./Sample_Report.json";
+import RightTopHeaderConatiner from "./RightTopHeader/RightTopHeaderConatiner";
 
 export default function MainPage() {
   const [articles, setArticles] = useState(Sample_Report.results);
 
+  const selectedArticle = articles[0];
+
+
   return (
-    <>
+    <Container>
       <Container>
-        <Container>
-          <Row className="container1">
-            <Col id="column1" lg={6} sm={12} xs={12} md={8}>
-              <Card >
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col id="column2" lg={2} sm={12} xs={12} md={4}>
-              xs=6 md=4
-            </Col>
-            <Col id="column3" lg={4} sm={12} xs={12} md={12}>
-              xs=6 md=4
-            </Col>
-            <RightTopHeaderConatiner/>
-          </Row>
-        </Container>
+        <Row className="container1">
+          <Col id="column1" lg={6} sm={12} xs={12} md={8}>
+            <Carousel>
+              {articles.map((article, index) => (
+                <Carousel.Item key={index}>
+                  {/* Assuming each article in the JSON file has an image_url and title */}
+                  <img
+                    className="d-block w-100"
+                    src={article.image_url}
+                    alt={article.title}
+                  />
+                  <Carousel.Caption>
+                    <h3>{article.title}</h3>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </Col>
+          <RightTopHeaderConatiner />
+        </Row>
       </Container>
-    </>
+    </Container>
   );
 }
